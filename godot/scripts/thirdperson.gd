700a8751ceb0edac69074a82e4f4d44a3ba388de
Jack Rosa <jackrosa183@gmail.com>
2025-11-24 19:18:04 -0600

    feat: camera movement

---
+++ godot/scripts/thirdperson.gd
@@ -0,0 +1,35 @@
+extends Node
+
+@onready var yaw_node = $CamYaw
+@onready var pitch_node = $CamYaw/CamPitch
+@onready var camera = $CamYaw/CamPitch/SpringArm3D/Camera3D
+
+var yaw: float = 0
+var pitch: float = 0
+
+var yaw_sensitivity: float = 0.07
+var pitch_sensitivity: float = 0.07
+
+var yaw_acceleration: float = 15
+var pitch_acceleration: float = 15
+
+var pitch_max = 75
+var pitch_min = -55
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += -event.relative.y * pitch_sensitivity
+
+func _physics_process(delta: float):
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
+	
