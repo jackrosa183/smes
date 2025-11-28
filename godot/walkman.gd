384220a658f898ef4dfc8399504095c4a4f16b0a
Jack Rosa <jackrosa183@gmail.com>
2025-11-28 14:04:49 -0600

    chore: player enter-exit tape zone

---
+++ godot/walkman.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+var can_pickup: bool = false
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		can_pickup = true
+		print("player entered") # Replace with function body.
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		can_pickup = false
+		print("player exited") 
