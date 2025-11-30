620a103671317cacbaf648efd2db03ca0a26ed15
Jack Rosa <jackrosa183@gmail.com>
2025-11-30 12:36:42 -0600

    chore: player hand attachment node

--- godot/walkman.gd
+++ godot/walkman.gd
@@ -10,5 +10,5 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		can_pickup = false
+		can_pickup = false	
 		print("player exited") 
